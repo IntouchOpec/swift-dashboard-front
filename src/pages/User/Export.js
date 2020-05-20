3a37e6ad06dc40266ca9f6d1c76f93cea13593dc
Sapjarern <sapjarern081@gmail.com>
2020-05-20 07:29:55 +0700

    Fix invalid output month in Export Page

--- src/pages/User/Export.js
+++ src/pages/User/Export.js
@@ -102,7 +102,7 @@ const UserExportPage = props => {
                         <thead>
                             <tr>
                                 <th>Staff Name : {data.full_name}</th>
-                                <th>Month : {yearfilter.value}</th>
+                                <th>Month : {monthfilter.label}</th>
                                 <th>Total Assigned : 2</th>
                             </tr>
                         </thead>
