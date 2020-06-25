d361c7fa06d60978ab8d583a112e005d5ed184cd
Sapjarern <sapjarern081@gmail.com>
2020-06-26 05:44:37 +0700

    report timesheet font

--- src/pages/User/Export.js
+++ src/pages/User/Export.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import client from 'utils/client'
-import { usersDetailURL } from 'utils/endpoint'
+import { timesheetReportURL } from 'utils/endpoint'
+import { styles } from 'utils/formats'
 import { Table, Button } from 'reactstrap'
 import { ChevronDoubleLeft } from 'react-bootstrap-icons'
-import Page from 'components/pdf/Export'
+import { Document, Page } from 'react-pdf';
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+// import Page from 'components/pdf/Export'
 import PrintButton from 'components/pdf/PrintButton'
 import SelectReact from 'components/forms/SelectReact'
+import moment from 'moment'
 
 const yearsOption = [
     { label: '2020', value: '2020' },
@@ -16,44 +22,55 @@ const yearsOption = [
 ]
 
 const monthsOption = [
-    { label: 'Jan', value: '1' },
-    { label: 'Feb', value: '2' },
-    { label: 'Mar', value: '3' },
-    { label: 'Apr', value: '4' },
-    { label: 'May', value: '5' },
-    { label: 'Jun', value: '6' },
-    { label: 'Jul', value: '7' },
-    { label: 'Aug', value: '8' },
-    { label: 'Sep', value: '9' },
-    { label: 'Oct', value: '10' },
-    { label: 'Nov', value: '11' },
-    { label: 'Dec', value: '12' },
+    { label: 'Jan', value: '01', day: 31 },
+    { label: 'Feb', value: '02', day: 29 },
+    { label: 'Mar', value: '03', day: 31 },
+    { label: 'Apr', value: '04', day: 30 },
+    { label: 'May', value: '05', day: 31 },
+    { label: 'Jun', value: '06', day: 30 },
+    { label: 'Jul', value: '07', day: 31 },
+    { label: 'Aug', value: '08', day: 31 },
+    { label: 'Sep', value: '09', day: 30 },
+    { label: 'Oct', value: '10', day: 31 },
+    { label: 'Nov', value: '11', day: 30 },
+    { label: 'Dec', value: '12', day: 31 },
 ]
 
-const FakeData = [
-    {code: '01.001', name: 'Mobilization Machine'},
-    {code: '02.001', name: 'Document Control'},
-    {code: '03.001', name: 'Piling Work'},
-    {code: '03.002', name: 'Footing Work'},
-    {code: '03.003', name: 'Column Work'},
-    {code: '04.001', name: 'Fire Alarm'},
-    {code: '04.002', name: 'Sanitary Work'},
-    {code: '04.003', name: 'Lighting'},
-    {code: '04.004', name: 'Earthing'},
-]
+const dateFormat = (date) => {
+    return moment(date).format("DD/MM/YYYY")
+}
 
 const UserExportPage = props => {
     const [data, setData] = useState({})
+    const [loading, setLoading] = useState(true)
     const [yearfilter, setYearFilter] = useState(yearsOption[0])
-    const [monthfilter, setMonthFilter] = useState(monthsOption[4])
-    useEffect(() => {
-        client.get(usersDetailURL.replace(':id', id))
+    const [monthfilter, setMonthFilter] = useState(monthsOption[parseInt(moment(Date.now()).format("M")) - 1])
+
+    /* useEffect(() => {
+        let date_after = `${yearfilter.value}-${monthfilter.value}-1`
+        let date_before = `${yearfilter.value}-${monthfilter.value}-${monthfilter.day}`
+        client.get(`${timesheetReportURL.replace(':id', id)}?start_data_after=${date_after}&start_data_before=${date_before}&end_date_after=${date_after}&end_date_before=${date_before}`)
             .then(res => {
                 setData(res.data)
+                console.log(res.data)
+                setLoading(false)
             }).catch(err => {
 
             })
-    }, [])
+    }, []) */
+    useEffect(() => {
+        setLoading(true)
+        let date_after = `${yearfilter.value}-${monthfilter.value}-1`
+        let date_before = `${yearfilter.value}-${monthfilter.value}-${monthfilter.day}`
+        client.get(`${timesheetReportURL.replace(':id', id)}?start_data_after=${date_after}&start_data_before=${date_before}&end_date_after=${date_after}&end_date_before=${date_before}`)
+            .then(res => {
+                setData(res.data)
+                console.log(res.data)
+                setLoading(false)
+            }).catch(err => {
+
+            })
+    }, [monthfilter, yearfilter])
 
     const onClickYearFilter = field => {
         setYearFilter(field)
@@ -62,7 +79,12 @@ const UserExportPage = props => {
         setMonthFilter(field)
     }
 
+    const exportPDF = data => {
+        return 0
+    }
+
     const { id } = useParams()
+
     return (
         <div className='mt-4'>
             <div className='row justify-content-between'>
@@ -88,86 +110,68 @@ const UserExportPage = props => {
                     />
                 </div>
                 <div className='col-sm-2 col-md-2'>
-                    <PrintButton id={`user-${id}`} label={'Save PDF'} />
+                    {/* <PrintButton id={`user-${id}`} label={'Save PDF'} /> */}
+                    <Button onClick={exportPDF} className='btn btn-primary'> Save PDF </Button>
                 </div>
                 <div className='col-sm-2 col-md-2'>
                     <Link to={`/users/${id}`}><Button className=''><ChevronDoubleLeft /> กลับ </Button></Link>
                 </div>
             </div>
             <hr />
-            <div className='row justify-content-center'>
-                <Page id={`user-${id}`}>
-                    <Table bordered className='p-4'>
-                        <thead>
-                            <tr>
-                                <th>Staff Name : {data.full_name}</th>
-                                <th>Month : {monthfilter.label}</th>
-                                <th>Total Assigned : 2</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th colSpan='3' align='center'>Job Name : {FakeData[0].name}</th>
-                            </tr>
-                        </tbody>
-                        <thead>
-                            <tr>
-                                <th align='center'>Date</th>
-                                <th colSpan='2' align='center'>Workdays</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td align='center'>18/05/2020 - 28/05/2020</td>
-                                <td colSpan='2' align='center'>40</td>
-                            </tr>
-                            <tr>
-                                <td align='center'>25/05/0202 - 26/05/2020</td>
-                                <td colSpan='2' align='center'>12</td>
-                            </tr>
-                            <tr>
-                                <td align='center'>Total</td>
-                                <td colSpan='2' align='center'>52</td>
-                            </tr>
-                        </tbody>
-                        <tbody>
-                            <tr>
-                                <th colSpan='3' align='center'>Job Name : {FakeData[1].name}</th>
-                            </tr>
-                        </tbody>
-                        <thead>
-                            <tr>
-                                <th align='center'>Date</th>
-                                <th colSpan='2' align='center'>Workdays</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td align='center'>26/05/2020 - 28/05/2020</td>
-                                <td colSpan='2' align='center'>20</td>
-                            </tr>
-                            <tr>
-                                <td align='center'>29/05/2020</td>
-                                <td colSpan='2' align='center'>8</td>
-                            </tr>
-                            <tr>
-                                <td align='center'>Total</td>
-                                <td colSpan='2' align='center'>28</td>
-                            </tr>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <td align='center'>Grand Total</td>
-                                <td colSpan='2' align='center'>80</td>
-                            </tr>
-                        </tfoot>
-                    </Table>
-                </Page>
-            </div>
-
-
+            {!loading &&
+                <Table bordered className='p-4'>
+                    <thead>
+                        <tr>
+                            <th>Staff Name : {data.user.fullname}</th>
+                            <th>Month : {monthfilter.label}</th>
+                            <th>Total Assigned : {data.time_sheets.length}</th>
+                        </tr>
+                    </thead>
+                    <RenderRow data={data.time_sheets} />
+                    <tfoot>
+                        <tr>
+                            <th align='center'>Grand Total</th>
+                            <td colSpan='2' align='center'>{sumallday(data.time_sheets)}</td>
+                        </tr>
+                    </tfoot>
+                </Table>
+            }
         </div>
     )
+}
+
+const RenderRow = props => {
+    const { data } = props
+    return data.map((jobs, jkeys) => {
+        return (
+            <tbody key={`Job-${jkeys}`}>
+                <tr><th colSpan='3'>Job Name : {jobs.job.name}</th></tr>
+                <tr><th colSpan='2'>Date</th><th>Workday</th></tr>
+                {jobs.time_sheets.map((job, jkey) => {
+                    return (
+                        <tr key={`${jkey}-${job.code}`}><td colSpan='2' align='center'>{`${dateFormat(job.start_date)} - ${dateFormat(job.end_date)}`}</td><td align='center'>{job.day}</td></tr>
+                    )
+                })}
+                <tr><th colSpan='2' align='center'>Total</th><td align='center'>{sumday(jobs.time_sheets)}</td></tr>
+            </tbody>
+        )
+    })
+}
+
+const sumday = timesheet => {
+    let sum = 0
+    timesheet.map((job) => {
+        sum += job.day
+    })
+    return sum
+}
+
+const sumallday = timesheet => {
+    let sum = 0
+    timesheet.map((jobs) => {
+        sum += sumday(jobs.time_sheets)
+    })
+    return sum
 }
 
 export default UserExportPage
