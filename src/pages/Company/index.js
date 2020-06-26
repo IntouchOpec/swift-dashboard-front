5858af0aa95e52312ff8241de0af50a6887f3357
Zaphkiel <napat.pns@gmail.com>
2020-06-26 11:43:20 +0700

    Company Delete

--- src/pages/Company/index.js
+++ src/pages/Company/index.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState, Component } from 'react'
 import TableBase from 'components/tables/TableBase'
-import { companyURL } from 'utils/endpoint'
+import { companyDetailURL, companyURL } from 'utils/endpoint'
 import { Button } from 'reactstrap'
 import { Link } from 'react-router-dom'
 import client from 'utils/client'
+import Swal from 'sweetalert2'
+import { useHistory } from 'react-router-dom'
 
 const LIMIT = 10
 
@@ -25,13 +27,23 @@ const FakeData = [
     {code: '008', name: 'Company H', user:'pao'},
     {code: '009', name: 'Company I', user:'pao'},
 ]
+  
 const RowRender = props => {
+    const history = useHistory()
     const [active, setActive] = useState(false)
-    
+    const [errors, setErrors] = useState({})
     useEffect(() => {
         setActive(props.active)
     }, [props.id])
     console.log(props)
+    function handleClick(id) {
+        console.log(id)
+        client.delete(companyDetailURL.replace(':id', id))
+        .then(res => {
+            Swal.fire('Created !', 'Success .', 'success')
+            .then(result => window.location.reload(false))
+        })
+    }
     return (
     <tr>
         <td>{props.code}</td>
@@ -41,7 +53,7 @@ const RowRender = props => {
         <Link to={`/company/edit/${props.id}`}>
             <Button className='rounded-0 mr-1' color='secondary'>edit</Button>
         </Link>
-        <Button color='danger'>Delete</Button></td>
+        <Button color='danger' onClick={() => handleClick(props.id)}>Delete</Button></td>
     </tr>
 )}
 /* const RowRender = props => {
